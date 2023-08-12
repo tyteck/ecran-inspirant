bb2c8dee438ba2a490ff9c977d34dcc32191c547
frederick tyteca <frederick@tyteca.net>
2023-08-12 12:32:12 +0200

    fix:typo

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('pageTitle') - Ecran Inspirant</title>
-    <meta name="description" content="{{ $decription }}">
+    <meta name="description" content="{{ $description }}">
     <meta name="keywords" content="Inspiration, fond d'écran, raccourcis, iphone, wallpaper">
     <meta name="author" content="Frederick Tyteca">
 
@@ -14,7 +14,7 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://ecran-inspirant.fr" />
     <meta property="og:image" content="https://ecran-inspirant.fr/images/welcome.jpg" />
-    <meta property="og:description" content="{{ $decription }}" />
+    <meta property="og:description" content="{{ $description }}" />
     <meta property="og:locale" content="fr_FR" />
 
     @vite(['resources/js/app.js'])
