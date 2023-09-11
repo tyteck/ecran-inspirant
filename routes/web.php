f9dba82e6d8b62cfd9673bbc280ef065b46b0ce3
frederick tyteca <frederick@tyteca.net>
2023-09-11 15:43:52 +0200

    custom error-page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\HelpController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuoteController;
+use App\Services\TailwindColors;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,14 @@ Route::domain('get.' . config('app.domain'))
 
 Route::get('/', [HomeController::class, 'show'])->name('index');
 Route::get('/aide', [HelpController::class, 'show'])->name('help');
+
+Route::fallback(function () {
+    $colorName = TailwindColors::init()->getOne()->name();
+    $message = __('messages.page_not_found');
+
+    return response()->view(
+        'errors.404',
+        compact('colorName', 'message'),
+        404
+    );
+});
